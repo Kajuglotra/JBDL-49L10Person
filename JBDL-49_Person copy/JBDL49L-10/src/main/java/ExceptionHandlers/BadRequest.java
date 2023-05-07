b5aab133868e1b29816a5a39b0d3d9f0e7e87f37
Kajuglotra <51613822+Kajuglotra@users.noreply.github.com>
2023-05-07 15:14:35 +0530

    Delete

--- JBDL-49_Person copy/JBDL49L-10/src/main/java/ExceptionHandlers/BadRequest.java
+++
@@ -1,7 +0,0 @@
-package ExceptionHandlers;
-
-public class BadRequest extends Exception{
-    public BadRequest(String msg){
-        super(msg);
-    }
-}
--- JBDL-49_Person copy/JBDL49L-10/src/main/java/org/gfg/JBDL49L10/repository/PersonJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.gfg.JBDL49L10.repository;
-
-import org.gfg.JBDL49L10.model.Myperson;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonJpaRepository extends JpaRepository<Myperson, Integer> {
-}
